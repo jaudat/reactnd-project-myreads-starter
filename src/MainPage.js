b40ec05f464979bc36ff5c08f4175ae40df7f1a2
Jaudat Syed <sjh.jaudat@gmail.com>
2017-06-30 03:40:14 -0400

    Got SearchPage to work with sanctioned terms

--- src/MainPage.js
+++ src/MainPage.js
@@ -24,9 +24,18 @@ export default class MainPage extends Component {
         </div>
         <div className="list-books-content">
           <div>
-            <BookShelf title='Currently Reading' books={this.state.books.filter( book => book.shelf === 'currentlyReading')}/>
-            <BookShelf title="Want to Read" books={this.state.books.filter( book => book.shelf === 'wantToRead')}/>
-            <BookShelf title="Read" books={this.state.books.filter( book => book.shelf === 'read')}/>
+            <BookShelf 
+              title='Currently Reading' 
+              books={this.state.books.filter( book => book.shelf === 'currentlyReading')}
+            />
+            <BookShelf 
+              title="Want to Read" 
+              books={this.state.books.filter( book => book.shelf === 'wantToRead')}
+            />
+            <BookShelf 
+              title="Read" 
+              books={this.state.books.filter( book => book.shelf === 'read')}
+            />
           </div>
         </div>
         <div className="open-search">
--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,19 +1,44 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import * as BooksAPI from './BooksAPI';
+import BookGrid from "./BookGrid"
 
 export default class SearchPage extends Component {
   
+  state = {
+    results: [],
+    query: ''
+  }
+
+  updateQuery(inputVal) {
+    var query = inputVal.trim()
+    if (!query) { 
+      this.setState({ query: '' , results: [] })
+    } else {
+      BooksAPI.search(query, 10).then(results => {
+        this.setState({ query , results })
+      });
+    }
+  }
+
   render() {
+    const query = this.state.query,
+          results = this.state.results;
     return (
       <div className="search-books">
         <div className="search-books-bar">
           <Link className="close-search" to="/">Close</Link>
           <div className="search-books-input-wrapper">
-            <input type="text" placeholder="Search by title or author"/>
+            <input 
+              type="text" 
+              placeholder="Search by title or author"
+              value={query}
+              onChange={ event => this.updateQuery.call(this, event.target.value) }
+            />
           </div>
         </div>
         <div className="search-books-results">
-          <ol className="books-grid"></ol>
+          <BookGrid books={results} />
         </div>
       </div>
     )
